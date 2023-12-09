ad07f591cfd9f1b9cf07c31b54cca856ccf6d546
minimeba <jc05326@naver.com>
2023-12-09 18:36:43 +0900

    경로 수정

--- calendar/static/js/main.js
+++ calendar/static/js/main.js
@@ -107,7 +107,7 @@ var calendar = $('#calendar').fullCalendar({
   events: function (start, end, timezone, callback) {
     $.ajax({
       type: "get",
-      url: "/data.json",
+      url: "/../data.json",
       data: {
         // 화면이 바뀌면 Date 객체인 start, end 가 들어옴
         //startDate : moment(start).format('YYYY-MM-DD'),
